e0a1baad2317855d523898e9ca45ba9b72e77f60
Power2All <power2all@gmail.com>
2022-09-16 13:11:58 +0200

    Debug builder

--- build.sh
+++ build.sh
@@ -1,23 +1,18 @@
 #!/bin/sh
 
-echo "###########################################################################"
 echo "Remove cache for clean build, without removing dynamic files like config..."
-echo "###########################################################################"
-
+echo ""
 rm -Rf target/release/.fingerprint target/release/build target/release/deps target/release/examples target/release/incremental target/release/.cargo-lock target/.rustc_info.json target/CACHEDIR.TAG
 
-echo "############################"
-echo "Build Torrust-Axum - Release"
-echo "############################"
-
+echo ""
 echo "The full web URL to your tracker API ( e.g. http://tracker.domain.com:8080 ):"
-read apiurl
-sed -i "s/{TRACKER_URL}/${apiurl}/g" webgui/index.htm
-
+read API_URL
+API_URL=$(echo ${API_URL} | sed -e "s#/#\\\/#g")
+sed -i "s/{TRACKER_URL}/${API_URL}/g" webgui/index.htm
+echo ""
 cargo build --release
-
-sed -i "s/${apiurl}/{TRACKER_URL}/g" webgui/index.htm
-
+sed -i "s/${API_URL}/{TRACKER_URL}/g" webgui/index.htm
+echo ""
 echo "#####################################################"
 echo "Building completed !"
 echo "You can find the compiled binary at ./target/release/"
