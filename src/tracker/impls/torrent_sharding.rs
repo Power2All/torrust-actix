6bad54dd21382de2e42323c6d87e37f25a781809
Power2All <power2all@gmail.com>
2024-07-11 23:42:36 +0200

    Debugging

--- src/tracker/impls/torrent_sharding.rs
+++ src/tracker/impls/torrent_sharding.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
 use std::sync::atomic::Ordering;
 use crossbeam_skiplist::SkipMap;
+use log::info;
 use crate::tracker::structs::info_hash::InfoHash;
 use crate::tracker::structs::torrent_entry::TorrentEntry;
 use crate::tracker::structs::torrent_sharding::TorrentSharding;
@@ -10,6 +11,7 @@ impl TorrentSharding {
     {
         let output = SkipMap::new();
         for count in 0u8..255u8 {
+            info!("{:#?}", count);
             output.insert(count, BTreeMap::new());
         }
         TorrentSharding {
@@ -19,6 +21,7 @@ impl TorrentSharding {
     }
 
     pub fn get(&self, info_hash: &InfoHash) -> Option<TorrentEntry> {
+        info!("{:#?}", &info_hash.0[0]);
         let binding = self.shards.get(&info_hash.0[0]).unwrap();
         let shard = binding.value();
         shard.get(info_hash).cloned()
