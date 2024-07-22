a903c352c4fb070768b082e0874a01c556d1c921
Power2All <power2all@gmail.com>
2024-07-22 14:16:52 +0200

    Moving back from Parking_Lot to SkipMap, too many deadlocks.

--- src/tracker/impls/torrent_tracker_torrents.rs
+++ src/tracker/impls/torrent_tracker_torrents.rs
@@ -1,5 +1,4 @@
 use std::collections::BTreeMap;
-use std::collections::btree_map::Entry;
 use std::sync::Arc;
 use log::info;
 use crate::stats::enums::stats_event::StatsEvent;
@@ -16,22 +15,14 @@ impl TorrentTracker {
         }
     }
 
-    pub fn add_torrent(&self, info_hash: InfoHash, torrent_entry: TorrentEntry) -> (TorrentEntry, bool)
+    pub fn add_torrent(&self, info_hash: InfoHash, torrent_entry: TorrentEntry) -> TorrentEntry
     {
         let shard = self.torrents_sharding.clone().get_shard(info_hash.0[0]).unwrap();
-        let mut lock = shard.write();
-        match lock.entry(info_hash) {
-            Entry::Vacant(v) => {
-                self.update_stats(StatsEvent::Torrents, 1);
-                (v.insert(torrent_entry).clone(), true)
-            }
-            Entry::Occupied(o) => {
-                (o.get().clone(), false)
-            }
-        }
+        let torrent = shard.get_or_insert(info_hash, torrent_entry).value().clone();
+        torrent
     }
 
-    pub fn add_torrents(&self, hashes: BTreeMap<InfoHash, TorrentEntry>) -> BTreeMap<InfoHash, (TorrentEntry, bool)>
+    pub fn add_torrents(&self, hashes: BTreeMap<InfoHash, TorrentEntry>) -> BTreeMap<InfoHash, TorrentEntry>
     {
         let mut returned_data = BTreeMap::new();
         for (info_hash, torrent_entry) in hashes.iter() {
@@ -43,14 +34,7 @@ impl TorrentTracker {
     pub fn get_torrent(&self, info_hash: InfoHash) -> Option<TorrentEntry>
     {
         let shard = self.torrents_sharding.clone().get_shard(info_hash.0[0]).unwrap();
-        let lock = shard.read_recursive();
-        lock.get(&info_hash).map(|torrent| TorrentEntry {
-            seeds: torrent.seeds.clone(),
-            peers: torrent.peers.clone(),
-            completed: torrent.completed,
-            updated: torrent.updated
-        })
-    }
+        shard.get(&info_hash).map(|torrent| torrent.value().clone())    }
 
     pub fn get_torrents(&self, hashes: Vec<InfoHash>) -> BTreeMap<InfoHash, Option<TorrentEntry>>
     {
@@ -64,31 +48,14 @@ impl TorrentTracker {
     pub fn remove_torrent(&self, info_hash: InfoHash) -> Option<TorrentEntry>
     {
         let shard = self.torrents_sharding.clone().get_shard(info_hash.0[0]).unwrap();
-        let mut lock = shard.write();
-        match lock.remove(&info_hash) {
-            None => { None }
-            Some(data) => {
-                self.update_stats(StatsEvent::Torrents, -1);
-                self.update_stats(StatsEvent::Seeds, data.seeds.len() as i64);
-                self.update_stats(StatsEvent::Peers, data.peers.len() as i64);
-                Some(data)
-            }
-        }
+        shard.remove(&info_hash).map(|data| data.value().clone())
     }
 
     pub fn remove_torrents(&self, hashes: Vec<InfoHash>) -> BTreeMap<InfoHash, Option<TorrentEntry>>
     {
         let mut returned_data = BTreeMap::new();
         for info_hash in hashes.iter() {
-            returned_data.insert(*info_hash, match self.remove_torrent(*info_hash) {
-                None => { None }
-                Some(torrent) => {
-                    self.update_stats(StatsEvent::Torrents, -1);
-                    self.update_stats(StatsEvent::Seeds, torrent.seeds.len() as i64);
-                    self.update_stats(StatsEvent::Peers, torrent.peers.len() as i64);
-                    Some(torrent)
-                }
-            });
+            returned_data.insert(*info_hash, self.remove_torrent(*info_hash));
         }
         returned_data
     }
