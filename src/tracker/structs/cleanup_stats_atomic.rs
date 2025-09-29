3dd1510851249befb934d53f21d91069fdc4a2c8
Power2All <power2all@gmail.com>
2025-09-29 08:36:43 +0200

    Revert "Possible sharding improvement"
    This reverts commit d5c09d701119394a8995be2ee4ae5e94fe60ee3b.

--- src/tracker/structs/cleanup_stats_atomic.rs
+++
@@ -1,7 +0,0 @@
-use crate::tracker::structs::padded_atomic_u64::PaddedAtomicU64;
-
-pub struct CleanupStatsAtomic {
-    pub torrents: PaddedAtomicU64,
-    pub seeds: PaddedAtomicU64,
-    pub peers: PaddedAtomicU64,
-}
